400c3130a53bf612d3018869bbbf9a25ace64448
Jon-Eric <67129297+jon-eric@users.noreply.github.com>
2020-06-19 17:15:38 -0500

    Implemented readmegen.py.

--- readmegen.py
+++ readmegen.py
@@ -2,29 +2,53 @@
 """Generate Examples in README.
 """
 import fileinput
+import random
 import re
+module = __import__('istanbul-setup')
 
 path = 'README.md'
 
-headerpat = re.compile(r'Examples?\n-+')
+examples = [[]]
 
-def generate(path=path):
+pattern = re.compile(r'Examples?\n-+\n.+$', re.DOTALL)
+
+def generate(path=path, examples=examples):
     # Read input.
     with open(path) as infile:
         contents = infile.read()
 
     # Regenerate examples.
-    contents = headerpat.sub('\n'.join(examplelines()), contents)
+    random.seed(0)
+    examples = '\n'.join(exampleslines(examples))
+    contents = pattern.sub(examples, contents)
 
     # Write output.
     with open(path, 'w') as outfile:
         outfile.write(contents)
 
-def examplelines():
-    yield mdheader('Example')
+def exampleslines(examples):
+    yield mdheader('Examples' if len(examples) > 1 else 'Example')
+    for args in examples:
+        yield from mdcode(examplelines(args))
+
+def examplelines(args):
+    # Shuffle places.
+    places = list(module.base_places)
+    random.shuffle(places)
+
+    # Example input.
+    yield ''
+    yield ' '.join(['$ istanbul-setup.py'] + args)
+
+    # Example output.
+    yield ''
+    yield from module.genboard(places)
 
 def mdheader(name):
     return name + '\n' + '-' * len(name)
+
+def mdcode(lines):
+    yield from (f'    {line}'.rstrip() for line in lines)
 
 if __name__ == '__main__':
     generate()
