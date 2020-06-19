e71f1d0ebd9511093ef1ef6bc98580f4f126dccf
Jon-Eric <67129297+jon-eric@users.noreply.github.com>
2020-06-19 12:57:11 -0500

    Added examplelines().

--- readmegen.py
+++ readmegen.py
@@ -6,16 +6,25 @@ import re
 
 path = 'README.md'
 
+headerpat = re.compile(r'Examples?\n-+')
+
 def generate(path=path):
     # Read input.
     with open(path) as infile:
         contents = infile.read()
 
     # Regenerate examples.
+    contents = headerpat.sub('\n'.join(examplelines()), contents)
 
     # Write output.
     with open(path, 'w') as outfile:
         outfile.write(contents)
+
+def examplelines():
+    yield mdheader('Example')
+
+def mdheader(name):
+    return name + '\n' + '-' * len(name)
 
 if __name__ == '__main__':
     generate()
