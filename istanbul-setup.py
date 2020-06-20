7d67d5594debff0f76085f9637f9bef6488012ca
Jon-Eric <67129297+jon-eric@users.noreply.github.com>
2020-06-19 19:05:21 -0500

    Added tea house, black market restriction.

--- istanbul-setup.py
+++ istanbul-setup.py
@@ -61,6 +61,15 @@ def legallayout(places):
     if not (0 < fountain[0] < row_max and 0 < fountain[1] < col_max):
        return False
 
+    # The Black Market (8) and the Tea House (9) must have a distance of at least
+    # 3 Places from each other and must not be placed in the same column or row.
+    black_market = divmod(places.index((8, 'Black Market')), width)
+    tea_house = divmod(places.index((9, 'Tea House')), width)
+    col_diff = abs(tea_house[1] - black_market[1])
+    row_diff = abs(tea_house[0] - black_market[0])
+    if col_diff + row_diff < 3 or col_diff < 1 or row_diff < 1:
+        return False
+
     return True
 
 def grouper(iterable, n):
