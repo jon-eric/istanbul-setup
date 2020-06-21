d943cbdfd37a7cb13231fbe658fa5d15ed8f4aeb
Jon-Eric <67129297+jon-eric@users.noreply.github.com>
2020-06-20 21:53:43 -0500

    Added assert.

--- test_istanbul-setup.py
+++ test_istanbul-setup.py
@@ -5,8 +5,9 @@ istanbul_setup = __import__('istanbul-setup')
 Board = functools.partial(istanbul_setup.Board, shuffle=False)
 
 def test_islegal():
-    assert Board().islegal()
-    assert not Board(mocha=True, letters=True).islegal()
+    assert Board().islegal(), "Unshuffled should be legal."
+    assert not Board(mocha=True).islegal(), "Unshuffled plus one expansion isn't legal due to tea house."
+    assert not Board(mocha=True, letters=True).islegal(), "Unshuffled Bazaar isn't legal due to fountain and tea house."
 
 def test_main():
     istanbul_setup.main([])
